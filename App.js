9688f7b53f8571be82fa618f4a178c5115584f7b
preqel <358795715@qq.com>
2018-10-10 15:58:17 +0800

    test

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
           */
 
           import React, {Component} from 'react';
-          import {Platform, StyleSheet, Text, View} from 'react-native';
+          import {Platform, StyleSheet,TextInput,Text, View} from 'react-native';
 
           const instructions = Platform.select({
           ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,14 +18,28 @@
 
           type Props = {};
           export default class App extends Component<Props> {
-          render() {
+
+
+              constructor() {
+                  super();
+                  this.state = {text:'Useless PlaceHolder'}
+              }
+
+              render() {
           return (
           <View style={styles.container}>
-          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.welcome}>preqel Welcome to React Native!</Text>
           <Text style={styles.instructions}>To get started, edit App.js</Text>
           <Text style={styles.instructions}>{instructions}</Text>
-          <TextInput style={{height:40}} placeholder={"type here"} >
-          </TextInput>
+              <View style={{flex:1,flexDirection:'column'}}>
+
+                  <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                  <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                  <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+
+              </View>
+              <TextInput style={{height:41}} value={this.state} onChangeText={(text=> this.setState(text))}/>
+              <TextInput style={{height:40}} value={this.state.text} onChangeText={(text)=>this.setState({text})}></TextInput>
 
       </View>
     );
